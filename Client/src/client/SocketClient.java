b2b67c688aadfea7267d64b802cc4a369c75d0ce
Farid A Marin Granados <faridmarin25@gmail.com>
2019-11-14 22:32:47 -0600

    User Input communication
    User Input communication from client side

--- Client/src/client/SocketClient.java
+++ Client/src/client/SocketClient.java
@@ -6,6 +6,7 @@ import java.io.ObjectOutputStream;
 import java.net.InetAddress;
 import java.net.Socket;
 import java.net.UnknownHostException;
+import java.util.Scanner;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -32,29 +33,41 @@ public class SocketClient {
         //get the localhost IP address, if server is running on some other IP, you need to use that
         InetAddress host = InetAddress.getLocalHost();
         //establish socket connection to server
+        
         myClientSocket = new Socket(host.getHostName(), 8080);
         BufferedReader in = null; 
         PrintWriter out = null; 
+     // Using Scanner for Getting Input from User 
+        Scanner personIn = new Scanner(System.in);
         while(true){
+        	
+        	try {
+        	
+        	//Gets input from user...:
+            System.out.println("Speak...:");
+        	String personInStr = personIn.nextLine(); 
                 
             //write to socket using ObjectOutputStream
             out = new PrintWriter(new OutputStreamWriter(myClientSocket.getOutputStream())); 
-            out.println("Client sends...: " + "Hello my frind I am the client"); 
+            out.println(personInStr); 
             out.flush(); 
              
              
             //read the server response message
          	in = new BufferedReader(new InputStreamReader(myClientSocket.getInputStream())); 
          	String serverResponse = (String) in.readLine(); 
-            System.out.println("Server Says :" + serverResponse);
+            System.out.println(serverResponse);
             
-            
-            
+        	}
+            catch(IOException ioe) 
+        	{   
             in.close(); 
             out.close(); 
+            personIn.close();
             myClientSocket.close(); 
             System.out.println("...Im done, good bye user. ");
             Thread.sleep(100);
+        	}
             
         }
     }
