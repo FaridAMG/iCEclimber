90594e0869758cb90e9d84ac4ce87b96a18a06d5
Java-Sith <prieto.luisdaniel@gmail.com>
2019-11-21 22:01:48 -0600

    Se arregla el Json de C

--- Client/C-Client/Client/json.c
+++ Client/C-Client/Client/json.c
@@ -19,33 +19,23 @@ json_object* createJson(int x1, int y1, int attack1, int vision1, int x2, int y2
 
     json_object_object_add(jobj1,"at", jint1);
 
-  /*Creating a json object*/
-
-    json_object * jobj2 = json_object_new_object();
-
   /*Creating a json integer*/
 
     json_object *jint2 = json_object_new_int(x1);
 
   /*Form the json object*/
 
-    json_object_object_add(jobj2,"equis", jint1);
+    json_object_object_add(jobj1,"equis", jint1);
 
   /*Creating a json object*/
 
-    json_object * jobj3 = json_object_new_object();
-
-  /*Creating a json integer*/
 
     json_object *jint3 = json_object_new_int(y1);
 
   /*Form the json object*/
 
-    json_object_object_add(jobj3,"ye", jint1);
+    json_object_object_add(jobj1,"ye", jint1);
 
-  /*Creating a json object*/
-
-    json_object * jobj4 = json_object_new_object();
 
   /*Creating a json integer*/
 
@@ -53,7 +43,7 @@ json_object* createJson(int x1, int y1, int attack1, int vision1, int x2, int y2
 
   /*Form the json object*/
 
-    json_object_object_add(jobj4,"vis", jint1);
+    json_object_object_add(jobj1,"vis", jint1);
 
 
   /*Creating a json array*/
@@ -63,21 +53,15 @@ json_object* createJson(int x1, int y1, int attack1, int vision1, int x2, int y2
   /*Creating json strings*/
 
     json_object *jstring1 = json_object_new_string(json_object_to_json_string(jobj1));
-    json_object *jstring2 = json_object_new_string(json_object_to_json_string(jobj2));
-    json_object *jstring3 = json_object_new_string(json_object_to_json_string(jobj3));
-    json_object *jstring4 = json_object_new_string(json_object_to_json_string(jobj4));
 
   /*Adding the above created json strings to the array*/
 
     json_object_array_add(jarray1,jstring1);
-    json_object_array_add(jarray1,jstring2);
-    json_object_array_add(jarray1,jstring3);
-    json_object_array_add(jarray1,jstring4);
 
 
   /*Creating a json object*/
 
-    json_object * jobj5 = json_object_new_object();
+    json_object * jobj2 = json_object_new_object();
 
   /*Creating a json integer*/
 
@@ -85,11 +69,8 @@ json_object* createJson(int x1, int y1, int attack1, int vision1, int x2, int y2
 
   /*Form the json object*/
 
-    json_object_object_add(jobj5,"at", jint5);
+    json_object_object_add(jobj2,"at", jint5);
 
-  /*Creating a json object*/
-
-    json_object * jobj6 = json_object_new_object();
 
   /*Creating a json integer*/
 
@@ -97,11 +78,8 @@ json_object* createJson(int x1, int y1, int attack1, int vision1, int x2, int y2
 
   /*Form the json object*/
 
-    json_object_object_add(jobj6,"equis", jint6);
+    json_object_object_add(jobj2,"equis", jint6);
 
-  /*Creating a json object*/
-
-    json_object * jobj7 = json_object_new_object();
 
   /*Creating a json integer*/
 
@@ -109,11 +87,8 @@ json_object* createJson(int x1, int y1, int attack1, int vision1, int x2, int y2
 
   /*Form the json object*/
 
-    json_object_object_add(jobj7,"ye", jint7);
+    json_object_object_add(jobj2,"ye", jint7);
 
-  /*Creating a json object*/
-
-    json_object * jobj8 = json_object_new_object();
 
   /*Creating a json integer*/
 
@@ -121,28 +96,21 @@ json_object* createJson(int x1, int y1, int attack1, int vision1, int x2, int y2
 
   /*Form the json object*/
 
-    json_object_object_add(jobj8,"vis", jint8);
+    json_object_object_add(jobj2,"vis", jint8);
 
     json_object *jarray2 = json_object_new_array();
 
 
   /*Creating json strings*/
 
-    json_object *jstring5 = json_object_new_string(json_object_to_json_string(jobj5));
-    json_object *jstring6 = json_object_new_string(json_object_to_json_string(jobj6));
-    json_object *jstring7 = json_object_new_string(json_object_to_json_string(jobj7));
-    json_object *jstring8 = json_object_new_string(json_object_to_json_string(jobj8));
+    json_object *jstring5 = json_object_new_string(json_object_to_json_string(jobj2));
 
   /*Adding the above created json strings to the array*/
 
-    json_object_array_add(jarray2,jstring5);
-    json_object_array_add(jarray2,jstring6);
-    json_object_array_add(jarray2,jstring7);
-    json_object_array_add(jarray2,jstring8);
+    json_object_array_add(jarray2,jstring2);
 
     json_object_object_add(jobj, "4", jarray1);
     json_object_object_add(jobj, "5", jarray2);
 
     return jobj;
-
 }
