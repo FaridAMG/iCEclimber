99147c50edfdcea0fbe4ccebe58986fb0afbd200
edusjor <solanojorge77@gmail.com>
2019-11-23 09:48:51 -0800

    Gui

---
+++ TryGame/cmake-build-debug/CMakeFiles/TryGame.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/TryGame.dir/main.c.o"
+  "TryGame"
+  "TryGame.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/TryGame.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
